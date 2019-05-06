13e3b6d920ffd0593a69ff3af004f22dec2019c9
Julian Pufler <puf17640@spengergasse.at>
2019-05-06 08:57:18 +0200

    less lines

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
+let welcome_text_list = [
+	"programming is my passion.",
+	"I love writing code.",
+	"I am 18 years old.",
+	"I am from Vienna.",
+	"I still go to school.",
+	"you can find me on GitHub.",
+]
+
 $(window, document, undefined).ready(()=>{
 	let rand_numbers;
+	let rand_count = 0;
+	
 	$.ajax({
-		url: "https://www.random.org/integers/?num=500&min=0&max=6&col=1&base=10&format=plain&rnd=new",
-		success: (data)=>{
-			rand_numbers = data.split("\n")
-			console.log(rand_numbers)
-		},
-		error: (e)=>{
-			console.error(e)
-		}
+		url: `https://www.random.org/integers/?num=500&min=0&max=${welcome_text_list.length-1}&col=1&base=10&format=plain&rnd=new`,
+		success: (data)=> rand_numbers = data.split("\n"),
+		error: console.error
 	})
-
 
 	let texttyping = (options) => {
 		let el = options["element"]
@@ -20,43 +25,34 @@ $(window, document, undefined).ready(()=>{
 		if(!options["remove_chars"])el.text("")
 		let functions = {
 			add: ()=>{
-							el.text(el.text()+txt[i])
-							if(i == txt.length-1){
-								el.attr("is-typing", false)
-								options["finished"]&&options["finished"]()								
-								return
-							}else
-								i++
-							setTimeout(functions["add"], speed+(Math.random()*(speed*0.2)-speed*0.1))
-						},
+				el.text(el.text()+txt[i])
+				if(i == txt.length-1){
+					el.attr("is-typing", false)
+					options["finished"]&&options["finished"]()								
+					return
+				}else
+					i++
+				setTimeout(functions["add"], speed+(Math.random()*(speed*0.2)-speed*0.1))
+			},
 			remove: ()=>{
-							let txt = el.text()
-							el.text(txt.substr(0, txt.length-1))
-							if(i == 1){
-								el.attr("is-typing", false)
-								options["finished"]&&options["finished"]()
-								return
-							}else
-								i--
-							setTimeout(functions["remove"], speed+(Math.random()*(speed*0.2)-speed*0.1))
-						}
+				let txt = el.text()
+				el.text(txt.substr(0, txt.length-1))
+				if(i == 1){
+					el.attr("is-typing", false)
+					options["finished"]&&options["finished"]()
+					return
+				}else
+					i--
+				setTimeout(functions["remove"], speed+(Math.random()*(speed*0.2)-speed*0.1))
+			}
 		}
 		setTimeout(functions[options["remove_chars"]?"remove":"add"], speed+(Math.random()*(speed*0.2)-speed*0.1))
 	}
 
-	let welcome_text_list = [
-		"programming is my passion.",
-		"this is my website.",
-		"I love writing code.",
-		"I am 17 years old.",
-		"I am from Vienna, Austria.",
-		"I still go to school.",
-		"you can find me on GitHub."
-	]
-
 	let show_welcome = ()=>{
 		var f = ()=>{
-			let msg = welcome_text_list[rand_numbers[Math.floor(Math.random()*500)]]
+			let msg = welcome_text_list[rand_numbers[(rand_count%500)]]
+			rand_count++
 			texttyping({
 				element: $("#welcome-text-2"),
 				speed: 100,
